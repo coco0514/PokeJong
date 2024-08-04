177c5a5f75e3350ab627149f42316b7fcb133202
suzuri <iranai53aka@gmail.com>
2024-08-04 15:08:44 +0900

    new

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 100738012,
+    wasmFileSize: 100750896,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
